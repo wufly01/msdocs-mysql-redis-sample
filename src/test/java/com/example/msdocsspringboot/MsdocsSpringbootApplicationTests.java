afd5377be565d65a7545cf1d96d557fb90e70d36
alan <wufly01@163.com>
2025-07-22 02:39:19 +0000

    mysql

---
+++ src/test/java/com/example/msdocsspringboot/MsdocsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msdocsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsdocsSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
